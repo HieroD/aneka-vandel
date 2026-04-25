6b62f1b5a09648576ec8c05681499597863b4d53
Hiero Deshardika <hiero.dhp@gmail.com>
2026-04-25 20:40:18 +0700

    add auth and guest middleware

--- pweb-prak/routes/web.php
+++ pweb-prak/routes/web.php
@@ -8,13 +8,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register', [RegisterController::class, 'create']);
-Route::post('/register/user', [RegisterController::class, 'store']);
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisterController::class, 'create']);
+    Route::post('/register/user', [RegisterController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
-Route::post('/login/user', [SessionController::class, 'store']);
-Route::delete('/logout', [SessionController::class, 'delete']);
+    Route::get('/login', [SessionController::class, 'create']);
+    Route::post('/login/user', [SessionController::class, 'store']);
+});
 
-
-
-
+Route::middleware('auth')->group(function () {
+    Route::delete('/logout', [SessionController::class, 'delete']);
+});
