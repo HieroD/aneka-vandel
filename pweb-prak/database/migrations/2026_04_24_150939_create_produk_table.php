0e75a5f2491265f6f5788f856698966520ac45c2
Hiero Deshardika <hiero.dhp@gmail.com>
2026-04-26 15:42:29 +0700

    create PemesananController, add jumlah column to produk table, revise detil_pemesanan jumlah to total_produk

--- pweb-prak/database/migrations/2026_04_24_150939_create_produk_table.php
+++ pweb-prak/database/migrations/2026_04_24_150939_create_produk_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->string('kategori');
             $table->integer('harga');
             $table->string('path_gambar');
+            $table->integer('jumlah');
             $table->timestamps();
         });
     }
