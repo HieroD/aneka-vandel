cfcc754c68fd42a74eb01606d9c2dcb117ac9ccc
Hannsu <mfarhanyanuara@gmail.com>
2026-04-27 15:51:47 +0700

    menambahkan beberapa page

---
+++ pweb-prak/resources/views/components/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./styles/home.css">
+</head>
+<body>
+    <!-- HEADER -->
+    <header>
+        <nav>
+            <div id="logo">
+                <a href="./index.html"><img src="./public/assets/logo-vandel.png" alt="logo"></a>
+                <span>Aneka Vandel</span>
+            </div>
+            <div id="navbar">
+                <a href="./public/about.html">About</a>
+                <a href="./public/catalog.html">Catalog</a>
+                <a href="./public/login.html"><button>Sign in</button></a>
+            </div>
+        </nav>
+    </header>
+
+</body>
+</html>
--- pweb-prak/resources/views/home.blade.php
+++ pweb-prak/resources/views/home.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>              
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-</body>
-</html>
+<x-layout>
+    <h1>Selamat Datang di Halaman Home</h1>
+    <p>Ini adalah halaman utama aplikasi Anda.</p>  
+</x-layout>
