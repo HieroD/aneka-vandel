2ce0d1ac2fb080feb220c9197ca08fb3cd097da9
Hiero <hiero.dhp@gmail.com>
2026-06-14 10:19:17 +0700

    pass  resource into home page view

--- pweb-prak/app/Http/Controllers/PageController.php
+++ pweb-prak/app/Http/Controllers/PageController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+
 class PageController extends Controller
 {
     public function home()
     {
-        return view('front.home');
+        $products = Product::latest()->take(4)->get();
+
+        return view('front.home', compact('products'));
+
     }
 
     public function about()
