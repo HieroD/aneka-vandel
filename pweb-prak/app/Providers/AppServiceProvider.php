518a216c86d2d5dd5970101c4334ba859421dda5
Hiero <hiero.dhp@gmail.com>
2026-07-08 21:34:17 +0700

    conf ke milyar tahun cahaya kali

--- pweb-prak/app/Providers/AppServiceProvider.php
+++ pweb-prak/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production' || isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+            URL::forceScheme('https');
+        }
+
         Gate::define('admin', function (User $user) {
             return $user->isAdmin() ? Response::allow() : Response::denyAsNotFound();
         });
