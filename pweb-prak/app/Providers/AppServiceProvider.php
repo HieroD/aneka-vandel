7a2d9e0208ae7dbb130ae73ea6916ac3831646c1
Hiero <hiero.dhp@gmail.com>
2026-07-09 21:41:33 +0700

    add brevo http package

--- pweb-prak/app/Providers/AppServiceProvider.php
+++ pweb-prak/app/Providers/AppServiceProvider.php
@@ -5,8 +5,12 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoApiTransport;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(HttpClientInterface::class, function ($app) {
+            return HttpClient::create();
+        });
     }
 
     /**
@@ -29,6 +35,13 @@ class AppServiceProvider extends ServiceProvider
 
         Gate::define('admin', function (User $user) {
             return $user->isAdmin() ? Response::allow() : Response::denyAsNotFound();
+        });
+
+        Mail::extend('brevo', function (array $config) {
+            return new BrevoApiTransport(
+                $config['key'],
+                app(HttpClientInterface::class),
+            );
         });
     }
 }
